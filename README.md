3d462c3dd90a8225b1ca85cd662a1c1b56e06ddd
AmandaKhol <amanda.camsua@gmail.com>
2026-01-22 11:28:38 +0100

    feat: add features

---
+++ README.md
@@ -0,0 +1,20 @@
+# NestJS Project
+
+work in progress...
+
+Project to practice NestJS.
+Includes:
+
+- db
+- 3 modules
+- serialization
+
+NOTE: find the file `request.http` in `src/users/` folder to try the app
+
+```bash
+npm run install
+```
+
+```bash
+npm run start:dev
+```
---
+++ src/users/users.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Get,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body.email, body.password);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+}
